b410e060b22cf1d92b178fcc14ad92a290dcf4df
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-05-17 13:30:41 +0530

    Update App.js
    new changes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import { CssBaseline, ThemeProvider} from "@mui/material";
 import { Topbar} from "./sences/global/topbar";
 import { useState, useEffect} from 'react';
 
+
+
 function App() {
   const [theme, ColorMode] = useMode();
   const [newtheme, newColorMode] = useMode();
   return (
-    
-    
+
     <ColorModeContext.Provider value={ColorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content">
-            
+          <main className="content">          
             <Topbar/>
-            
-            
-            
-            
-            
-            
           </main>
         </div>
       </ThemeProvider>
