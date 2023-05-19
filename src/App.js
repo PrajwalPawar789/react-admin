c6bc6f2a84f4f7845198bc383b4ba8b0bbe3e996
Prajwal Pawar <114561900+PrajwalPawar789@users.noreply.github.com>
2023-05-19 19:03:17 +0530

    Update App.js
    updated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { CssBaseline, ThemeProvider} from "@mui/material";
 import { Topbar} from "./sences/global/topbar";
 import { useState, useEffect} from 'react';
 
-
-
-
 function App() {
   const [theme, ColorMode] = useMode();
   const [newtheme, newColorMode] = useMode();
